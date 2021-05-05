602fcd5a0ca4fc5bcf96af221059ef19aeeaf3e6
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-05-05 23:58:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const uri = mongodb+srv://lekandev:<password>@cluster0.sbgkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-const port = 9010;
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.create('/create', (req, res) => res.json({ requestType: 'create'}));
+
+app.get('/', (req, res) => res.json({ requestType: 'create'}));
+
+app.put('/update/:id', (req, res) => res.json({ requestType: 'create'}));
+
+app.delete('/delete/:id', (req, res) => res.json({ requestType: 'create'}));
+
+const PORT = process.env.PORT || 9010;
+
+app.listen(PORT, () => console.log(`Servver don start at ${PORT}`));
