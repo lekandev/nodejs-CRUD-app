2ea4466f3efda3cf5a1b1c545ae3ef3bac940601
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-05-06 03:19:50 +0100

    install express and mongoose

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const uri = mongodb+srv://lekandev:<password>@cluster0.sbgkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const uri = "mongodb+srv://lekandev:<password>@cluster0.sbgkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.create('/create', (req, res) => res.json({ requestType: 'create'}));
+// app.create('/create', (req, res) => res.json({ requestType: 'create'}));
 
-app.get('/', (req, res) => res.json({ requestType: 'create'}));
+// app.get('/', (req, res) => res.json({ requestType: 'create'}));
 
-app.put('/update/:id', (req, res) => res.json({ requestType: 'create'}));
+// app.put('/update/:id', (req, res) => res.json({ requestType: 'create'}));
 
-app.delete('/delete/:id', (req, res) => res.json({ requestType: 'create'}));
+// app.delete('/delete/:id', (req, res) => res.json({ requestType: 'create'}));
 
 const PORT = process.env.PORT || 9010;
 
-app.listen(PORT, () => console.log(`Servver don start at ${PORT}`));
+app.listen(PORT, () => console.log(`Server don start at ${PORT}`));
